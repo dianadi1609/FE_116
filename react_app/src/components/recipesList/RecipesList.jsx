05637b49b37e08a9017c267f8f9ba751c5d9d8f6
Your Name <you@example.com>
2023-12-01 20:22:37 +0200

    FIX

--- react_app/src/components/recipesList/RecipesList.jsx
+++ react_app/src/components/recipesList/RecipesList.jsx
@@ -1,49 +1,52 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const baseURL = "https://api.edamam.com/api/recipes/v2";
 const type = "public";
+const query = "popular"
 const apiKey = "e0726941d4b3edb3980f1cb5f23ac4b2";
-const apiId = "ea0d8161"; 
-function RecipesList () {
-        const [recipes, setRecipes] = useState([null]);
-        const [error, setError] = useState([null]);
-        useEffect(() => {
-            axios.get(baseURL, {
-                params: {
-                  type: type,
-                  app_id: apiId,
-                  app_key: apiKey
-                }})
-                  .then(response => {
-                    setRecipes(response.data.results);
-                    console.log(response.data)
-                  })
-                  .catch(error => {
-                    setError(error.message);
-        })
-         }, []);
+const apiId = "ea0d8161";
 
-         if (error)  {
+function RecipesList() {
+  const [recipes, setRecipes] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios.get(baseURL, {
+      params: {
+        type: type,
+        app_id: apiId,
+        app_key: apiKey,
+        q: query
+      }
+    })
+        .then(response => {
+          setRecipes(response.data.hits);
+        })
+        .catch(error => {
+          setError(error);
+        })
+  }, []);
+
+  if (error) {
     return <div className="error">
       <h2>{error}</h2>
     </div>;
   } else if (recipes) {
-    console.log(recipes)
-    const items = recipes.map((recipe, index) =>
+    const items = recipes.map((item, index) =>
         <div key={index} className="recipe">
           <Link to={"/recipes/"} className="link">
-            <h2>{recipe.title}</h2>
-            <p>{recipe.overview}</p>
+            <h2>{item.recipe.label}</h2>
+            <img src={item.recipe.image}/>
           </Link>
         </div>
     );
-    return <div className= "reciprs">
+    return <div className="reciprs">
       {items}
     </div>;
-}
+  }
 
 }
+
 export default RecipesList;
