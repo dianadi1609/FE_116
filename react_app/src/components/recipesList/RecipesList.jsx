09cc1ae0c8103d3bd4a4be92359e1e7b96add89b
Your Name <you@example.com>
2023-12-26 15:06:39 +0200

    add like

--- react_app/src/components/recipesList/RecipesList.jsx
+++ react_app/src/components/recipesList/RecipesList.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import React from "react";
 import {Link} from "react-router-dom";
+import { ReactComponent as Like } from "../../assets/img/like.svg";
 
 const baseURL = "https://api.edamam.com/api/recipes/v2";
 const type = "public";
@@ -12,6 +13,36 @@ const apiId = "ea0d8161";
 function RecipesList() {
   const [recipes, setRecipes] = useState(null);
   const [error, setError] = useState(null);
+  const [liked, setLiked] = useState(() => {
+    let items = [];
+    for(let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (!key.indexOf('label-')) {
+        items.push(key.replace('label-',''));
+      }
+    }
+    return items;
+  });
+
+  const setWishList = (event) => {
+     event.preventDefault();
+     let label = event.target.parentNode.parentNode.attributes.getNamedItem('data-label').value;
+     let isFavourited = liked.includes(label)
+     if (!isFavourited) {
+       let newItem = [...liked, label]
+       setLiked(newItem);
+       for (let i = 0; i < recipes.length; i++) {
+         if (recipes[i].recipe.label == label) {
+           window.localStorage.setItem('label-' + label, JSON.stringify(recipes[i]));
+         }
+       }
+     } else {
+       let newItem = liked.filter((savedId) => savedId !== label)
+       console.log(newItem)
+       setLiked(newItem);
+       window.localStorage.removeItem('label-' + label, );
+     }
+  }
   useEffect(() => {
     axios.get(baseURL, {
       params: {
@@ -37,6 +68,9 @@ function RecipesList() {
     const items = recipes.map((item, index) =>
         <div key={index} className="recipe">
           <Link to={"/recipes/"} className="link">
+            <div onClick={setWishList} data-label={item.recipe.label} className={liked.includes(item.recipe.label) ? "wishlist liked" : "wishlist"}>
+              <Like></Like>
+            </div>
             <h2>{item.recipe.label}</h2>
             <p className="ingredients">
             <b>Ingredients: </b> 
